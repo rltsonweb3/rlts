e596fe1ea78be538b53fc7e4831748c45555f120
rltsonweb3 <rltsonweb3@outlook.com>
2022-08-10 22:47:00 +0800

    add next.js for rlts-web

---
+++ rlts-web/pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>RLTS On IPFS/Filecoin</title>
+        <meta name="description" content="RLTS on IPFS/Filecoin" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://www.iucnredlist.org/" target={"_blank"}>Global Ecological Protection!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Build on
+          <code className={styles.code}>IPFS/Filecoin</code>
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
